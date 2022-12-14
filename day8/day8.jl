96ebc96b5cad46c61876115babac5bdc2b3ed03b
Marcel Endres <marcel.endres@globalclimateforum.org>
2022-12-14 11:28:53 +0100

    second challange day 8

--- day8/day8.jl
+++ day8/day8.jl
@@ -72,6 +72,59 @@ function getVisibleCount(grid::Matrix{Int})
     return counter
 end
 
+function getLeftScenicScore(x::Int, y::Int, grid::Matrix{Int})
+    for i in reverse(1:y-1)
+        if grid[x,y] <= grid[x,i]
+            return y-i
+        end
+    end
+    return y-1
+end
+
+function getRightScenicScore(x::Int, y::Int, grid::Matrix{Int})
+    for i in y+1:size(grid)[2]
+        if grid[x,y] <= grid[x,i]
+            return i-y
+        end
+    end
+    return size(grid)[2]-y
+end
+
+function getUpperScenicScore(x::Int, y::Int, grid::Matrix{Int})
+    for i in reverse(1:x-1)
+        if grid[x,y] <= grid[i,y]
+            return x-i
+        end
+    end
+    return x-1
+end
+
+function getDownScenicScore(x::Int, y::Int, grid::Matrix{Int})
+    for i in x+1:size(grid)[1]
+        if grid[x,y] <= grid[i,y]
+            return i-x
+        end
+    end
+    return size(grid)[1]-x
+end
+
+function getScenicScore(x::Int, y::Int, grid::Matrix{Int})
+    return getLeftScenicScore(x, y, grid) * getRightScenicScore(x, y, grid) * getUpperScenicScore(x, y, grid) * getDownScenicScore(x, y, grid)
+     
+end
+
+function getMaxScenicScore(grid::Matrix{Int})
+    maxScore::Int = 0
+    for i in 1:size(grid)[1]
+        for j in 1:size(grid)[2]
+            if isVisible(i, j, grid) && getScenicScore(i,j,grid) > maxScore
+                maxScore = getScenicScore(i,j,grid)
+            end
+        end
+    end
+    return maxScore
+end
+
 function main()
     grid::Matrix{Int} = Matrix{Int}(undef, 0, 0)
     open("day8/day8_input", "r") do input
@@ -86,5 +139,8 @@ function main()
     print("There are ")
     print(getVisibleCount(grid))
     println(" trees visible")
+
+    print("The best Tree has a scenic score of: ")
+    print(getMaxScenicScore(grid))
 end
 main()
--- day8/testDay8.jl
+++ day8/testDay8.jl
@@ -9,3 +9,7 @@ grid::Matrix{Int} = Matrix{Int}([3 0 3 7 3; 2 5 5 1 2; 6 5 3 3 2; 3 3 5 4 9; 3 5
 @test isVisible(1, 1, grid) == true
 @test isVisible(2, 4, grid) == false
 @test getVisibleCount(grid) == 21 
+
+@test getScenicScore(2, 3, grid) == 4
+@test getScenicScore(4, 3, grid) == 8
+
