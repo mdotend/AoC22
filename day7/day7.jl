46e9e08956834ca8c595f8114ef60e62a8655338
Marcel Endres <marcel.endres@globalclimateforum.org>
2022-12-14 11:29:04 +0100

    day7 experiments

--- day7/day7.jl
+++ day7/day7.jl
@@ -1,9 +1,12 @@
 abstract type Node end
 
-struct Directory <: Node
+mutable struct Directory <: Node
     name::String
     parent::Node
     children::Vector{Node}
+    Directory(name::String) = (dir = new(); dir.name = name; dir.children = Vector{Node}(); dir)
+    Directory(name::String, parent::Node) = (dir = new(); dir.name = name; dir.parent = parent; dir.children = Vector{Node}(); dir)
+    Directory(name::String, parent::Node, children::Vector{Node}) = new(name, parent, children)
 end
 
 struct File <: Node
@@ -12,16 +15,84 @@ struct File <: Node
     size::Int
 end
 
+function getParent(node::Node)
+    return node.parent
+end
+
 function isCommand(input::String)
     commandPattern::Regex = r"^\$.*"
     return match(commandPattern, input) != ""
 end
 
-function parseCommand!(input::String, workingNode::Node)
-    command::Vector{String} = split(input, " ")
+function getDirectoryFromCommand!(workingNode::Directory, input::String)
+    return getDirectoryFromCommand!(workingNode, [input])
+end
+
+function getDirectoryFromCommand!(workingNode::Directory, input::Vector{String})
+    command::Vector{String} = split(input[1], " ")
     if command[2] == "cd"
         if workingNode.name == ""
-            workingNode.Name = command[3]
+            workingNode.name = command[3]
+        end
+        if command[3] == "/"
+            while workingNode.name != "/"
+                workingNode = getParent(workingNode)
+            end
+        elseif command[3] == ".."
+            return getParent(workingNode)
+        else
+            for child in workingNode.children
+                if child.name == command[3]
+                    return child
+                end
+            end
+            throw("Directory " * command[3] * " not found")
+        end
+    elseif command[2] == "ls"
+        for line in input[2:end]
+            (first::String, second::String) = split(line, " ", limit=2)
+            if first == "dir"
+                push!(workingNode.children, Directory(second, workingNode))
+            else
+                push!(workingNode.children, File(second, workingNode, parse(Int, first)))
+            end
+        end
+    end
+    return workingNode
+end
+
+function getSize(dir::Directory)
+    size::Int = 0
+    for child in dir.children
+        size += getSize(child)
+    end
+    return size
+end
+
+function getSize(file::File)
+    return file.size
+end
+
+function main()
+    fileSystem::Directory = Directory("")
+    open("day7/day7_input.txt", "r") do input
+        currentNode = currentNode::Node = getDirectoryFromCommand!(fileSystem, raw"$ cd /")
+        commandInput = readlines(input)
+        counter::Int = 2
+        while counter <= length(commandInput)
+            commands::Vector{String} = Vector{String}()
+            if isCommand(commandInput[counter])
+                push!(commands, commandInput[counter])
+                counter += 1
+                if occursin("ls ", commands[1])
+                    while !isCommand(commandInput[counter])
+                        push!(commands, commandInput[counter])
+                        counter += 1
+                    end
+                end
+                currentNode = getDirectoryFromCommand!(currentNode, commands)
+            end
         end
     end
 end
+#main()
--- day7/testDay7.jl
+++ day7/testDay7.jl
@@ -3,6 +3,29 @@ include("day7.jl")
 
 @test isCommand(raw"$ cd /") == true
 
-currentNode = Node(undef, "")
-parseCommand!(raw"$ cd /")
-@test currentNode == Node(undef, "/")
+fileSystem = Directory("")
+currentNode::Node = getDirectoryFromCommand!(fileSystem, raw"$ cd /")
+@test currentNode.name == "/"
+@test length(currentNode.children) == 0
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d")
+@test length(currentNode.children) == 4
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ cd a")
+@test currentNode.name == "a"
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst")
+@test length(currentNode.children) == 4
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ cd ..")
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ cd d")
+currentNode = getDirectoryFromCommand!(currentNode, raw"$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k")
+@test getSize(currentNode) == 24933642
